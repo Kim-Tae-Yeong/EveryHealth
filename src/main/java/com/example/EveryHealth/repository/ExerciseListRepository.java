6cc7e97c25e23a5ebfd84f07a34e97d55c01e89d
Handongbeom <zzx010319@naver.com>
2024-11-23 18:08:29 +0900

    Chore: ExerciseListRepository 수정

--- src/main/java/com/example/EveryHealth/repository/ExerciseListRepository.java
+++ src/main/java/com/example/EveryHealth/repository/ExerciseListRepository.java
@@ -1,18 +1,10 @@
 package com.example.EveryHealth.repository;
 
 import com.example.EveryHealth.entity.ExerciseListEntity;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExerciseListRepository extends JpaRepository<ExerciseListEntity, Long> {
-
-    /**
-     * 모든 운동 데이터를 Slice로 가져옵니다.
-     * @param pageable 페이징 정보를 포함하는 객체
-     * @return 운동 데이터의 Slice
-     */
-    Slice<ExerciseListEntity> findAllBy(Pageable pageable);
+    // 기본 제공되는 CRUD 메서드 사용
 }
