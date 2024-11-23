fdbf0489ca9dd1f9c2ba523dad051dbd1bf51e1d
Handongbeom <zzx010319@naver.com>
2024-11-23 18:10:33 +0900

    Feat: ExerciseListDTO 추가

---
+++ src/main/java/com/example/EveryHealth/dto/ExerciseListDTO.java
@@ -0,0 +1,37 @@
+package com.example.EveryHealth.dto;
+
+import com.example.EveryHealth.entity.ExerciseListEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExerciseListDTO {
+
+    private Long exerciseListId;
+    private String exerciseName;
+    private String categorize;
+    private String exerciseListImageUrl;
+    private String description;
+
+    /**
+     * Entity -> DTO 변환 메서드
+     * @param exerciseListEntity ExerciseListEntity 객체
+     * @return ExerciseListDTO 객체
+     */
+    public static ExerciseListDTO toExerciseListDTO(ExerciseListEntity exerciseListEntity) {
+        ExerciseListDTO dto = new ExerciseListDTO();
+        dto.setExerciseListId(exerciseListEntity.getExerciseListId());
+        dto.setExerciseName(exerciseListEntity.getExerciseName());
+        dto.setCategorize(exerciseListEntity.getCategorize());
+        dto.setExerciseListImageUrl(exerciseListEntity.getExerciseListImageUrl());
+        dto.setDescription(exerciseListEntity.getDescription());
+        return dto;
+    }
+}
